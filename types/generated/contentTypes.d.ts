3304247c91c02bb628b70ff23fd255a728ad3edb
parpaploy <ramita1846@gmail.com>
2025-08-08 10:10:55 +0700

    update:facility-status

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -416,6 +416,9 @@ export interface ApiFacilityFacility extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
+    available: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<true>;
     contact_link: Schema.Attribute.String & Schema.Attribute.Required;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
