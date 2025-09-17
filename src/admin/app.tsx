df8b65ba49bc8846efec8ab03c75785e6b18d159
ICAT-cnx <icat.cnx@gmail.com>
2025-09-17 18:32:49 +0700

    Create app.tsx

---
+++ src/admin/app.tsx
@@ -0,0 +1,16 @@
+export default {
+  config: {
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Welcome to DITC!",
+        "Auth.form.welcome.subtitle": "Log in to continue",
+        // optionally change the login button text
+        "Auth.form.button.login.strapi": "Sign in",
+      },
+      // you can also add other locales like 'fr', 'es', etc.
+    },
+  },
+  bootstrap(app: any) {
+    // optional bootstrap code
+  },
+};
